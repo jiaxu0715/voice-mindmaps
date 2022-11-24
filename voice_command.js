a0ba744989fafa274f42f550b20ac371ee0406dc
jiaxu0715 <jiaxu0715@gmail.com>
2022-11-24 21:52:35 +0900

    initial commit

---
+++ voice_command.js
@@ -0,0 +1,53 @@
+
+
+//MINDMAP
+  var mind = {
+    "meta": {
+        'name': 'example',
+        'version': '1'
+    },
+    'format': 'node_array',
+    'data': []
+};
+var options = {
+    container: 'jsmind_container',
+    theme: 'success',
+    editable: true,
+};
+    
+var jm = new jsMind(options);
+
+
+
+function appendRoot(main){
+    //console.log (topic);
+    mind.data.push({ "id": 'root', 'isroot': true, "topic": main });
+    jm.show(mind);
+}
+
+function appendNode(topic){
+    //console.log (topic);
+    mind.data.push({ "id": topic, 'parentid': 'root', "topic": topic });
+    jm.show(mind);
+}
+//VOICE COMMANDS
+if (annyang) {
+    
+    const commands = {
+      'the main idea is *main': appendRoot,
+      'new idea *topic': appendNode,
+        'This is something': () => {alert ('something')},
+    };
+   
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+  
+    // Start listening.
+    SpeechKITT.annyang();
+
+    // Define a stylesheet for KITT to use
+    SpeechKITT.setStylesheet('//cdnjs.cloudflare.com/ajax/libs/SpeechKITT/0.3.0/themes/flat-midnight-blue.css');
+  
+    // Render KITT's interface
+    SpeechKITT.vroom();
+  }
