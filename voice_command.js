376910f43815d63edee74eaf8600ed2a8b9b8baa
jiaxu0715 <jiaxu0715@gmail.com>
2022-12-07 23:06:00 +0900

    removed unnecessary things

--- voice_command.js
+++ voice_command.js
@@ -30,28 +30,28 @@ function appendNode(topic) {
     mind.data.push({ "id": topic, 'parentid': 'root', "topic": topic });
     jm.show(mind);
 }
+appendRoot ("Programming");
+annyang.addCallback('result', function(phrases) {
+    console.log("I think the user said: ", phrases[0]);
+    console.log("But then again, it could be any of the following: ", phrases);
+    document.getElementById("results").innerHTML = "What's being heard: " + phrases;
+  });
+
 //VOICE COMMANDS
 if (annyang) {
 
     const commands = {
         '(the) main idea is *main': appendRoot,
-        'new idea *topic': appendNode,
+        '(add) new idea *topic': appendNode,
         'Append node *topic': appendNode,
         'New node *topic': appendNode,
+        //'New idea adding on to '
+        //for testing December 8th
         'Hello': () => { alert('Hello there! Did you say something?') },
     };
 
     // Add our commands to annyang
     annyang.addCommands(commands);
-
-    annyang.addCallback("error", function () {
-        $(".myErrorText").text("There was an error!");
-    });
-    annyang.addCallback("resultMatch", function (userSaid, commandText, phrases) {
-        console.log(userSaid);
-        console.log(commandText);
-        console.log(phrases);
-    });
     // Start listening.
     SpeechKITT.annyang();
 
@@ -61,4 +61,3 @@ if (annyang) {
     // Render KITT's interface
     SpeechKITT.vroom();
 }
-
